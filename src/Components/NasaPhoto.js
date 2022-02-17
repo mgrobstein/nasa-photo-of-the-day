684593348856b88794bf76b8de174f6fe13b73f2
mgrobstein <97206147+mgrobstein@users.noreply.github.com>
2022-02-17 15:47:25 -0800

    Did some styling but then the API got mad that I had done too many requests so I basically have to stop for now

--- src/Components/NasaPhoto.js
+++ src/Components/NasaPhoto.js
@@ -1,15 +1,28 @@
 import React from 'react';
+import styled, { css } from 'styled-components';
+const StyledNasaPhoto = styled.div`
+border: 10px solid;
+border-color: coral;
+background-color: ${props => props.color ? props.color : "lemonchiffon"};
 
-const NasaPhoto = (props) => {
+h3{
+    font-size: 8rem;
+}
+
+p{
+    font-size: 2rem;
+}
+`
+function NasaPhoto(props) {
     return (
-        <div className="nasa-photo-wrapper">
+        <StyledNasaPhoto className="nasa-photo-wrapper">
             <h3>{props.photo.title}</h3>
             <p>{props.photo.date}</p>
-            <img src={props.photo.url}/>
-            <p className = "explanation">{props.photo.explanation}</p>
-        </div>
+            <img src={props.photo.url} />
+            <p className="explanation">{props.photo.explanation}</p>
+        </StyledNasaPhoto>
 
-    )
+    );
 }
 
 export default NasaPhoto;
